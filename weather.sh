38fff7b1fe92bb958318d47273caedbd0376a1d8
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2024-11-29 13:50:19 +0000

    into the right folder but saving as today. need to save it as timestamp

--- weather.sh
+++ weather.sh
@@ -2,6 +2,7 @@
 
 date
 echo "Downloading weather data..."
-wget -O `date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today>>/data/weather    
+wget -P data/weather/ `date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
 echo "Weather data downloaded."
 date
+
