2acfa1b0eb826227d74645c4f4d2773ed9c90ac8
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2024-12-17 09:41:58 +0000

    weather_automation.py started

---
+++ Notebook/data_notebook/weather_automation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import datetime as dt
+import os
+import http
+
+df = pd.read_html('https://data.gov.ie/dataset/todays-weather-athenry')
+
+filename = dt.datetime.now()
+filename = filename.strftime("%Y%m%d_%H%M%S")
+filename = 'data_notebook/' + filename + ".csv"
+
+os.makedirs('data_notebook', exist_ok=True)
+
+df[0].to_csv(filename)
+
